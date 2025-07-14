ee231aa55a67f0612e5446e25f2dcb087bdb7e38
juliaomedeiros <juliaomedeiros@users.noreply.github.com>
2025-07-14 14:55:49 +0000

    deploy: 8dc11de72a6286f664f741a1fbf7ba0b63501df5

---
+++ env.js
@@ -0,0 +1,5 @@
+const ENV = {
+  SUPABASE_URL: 'https://yqqpugheqqknpbetysme.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxcXB1Z2hlcXFrbnBiZXR5c21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAxOTkwNTUsImV4cCI6MjA2NTc3NTA1NX0.Q89vTdLgodaIsuLiIB6JijJPuzyrcRNPoTwUJ_gUQV4'
+};
+window.ENV = ENV;
